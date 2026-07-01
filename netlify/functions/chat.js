49ca8c8a24f505dfed99fb74c26de962c820301b
Badju03 <lyndon_gmz@yahoo.com>
2026-07-01 14:05:49 +0800

    Add files via upload

---
+++ netlify/functions/chat.js
@@ -0,0 +1,40 @@
+const knowledge = require("./knowledge");
+const sessions = require("./sessions");
+
+function startConversation(question) {
+
+    const lower = question.toLowerCase();
+
+    for (const category in knowledge) {
+
+        const item = knowledge[category];
+
+        const found = item.keywords.some(keyword =>
+            lower.includes(keyword)
+        );
+
+        if (found) {
+
+            sessions.current = {
+
+                category,
+
+                step: 1
+
+            };
+
+            return item.steps[0].question;
+
+        }
+
+    }
+
+    return "Sorry, I don't recognize that issue yet. Try Outlook or Email.";
+
+}
+
+module.exports = {
+
+    startConversation
+
+};
