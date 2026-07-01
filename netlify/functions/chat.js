5da7ab0baaf80a60cc5b785254b91c7e298cf8cc
Badju03 <lyndon_gmz@yahoo.com>
2026-07-01 14:35:53 +0800

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -1,43 +1,29 @@
 const engine = require("./engine");
 
 exports.handler = async (event) => {
-    
-};
 
-function startConversation(question) {
+    try {
 
-    const lower = question.toLowerCase();
+        const { question } = JSON.parse(event.body);
 
-    for (const category in knowledge) {
+        const answer = engine.startConversation(question);
 
-        const item = knowledge[category];
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                answer
+            })
+        };
 
-        const found = item.keywords.some(keyword =>
-            lower.includes(keyword)
-        );
+    } catch (err) {
 
-        if (found) {
-
-            sessions.current = {
-
-                category,
-
-                step: 1
-
-            };
-
-            return item.steps[0].question;
-
-        }
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                answer: err.message
+            })
+        };
 
     }
-
-    return "Sorry, I don't recognize that issue yet. Try Outlook or Email.";
-
-}
-
-module.exports = {
-
-    startConversation
 
 };
